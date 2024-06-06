503eaf9f0f946fb815f34f0263e82f38d7c00abe
Claiton Watthier <cla.cgw@gmail.com>
2024-06-05 21:58:42 -0300

    test

---
+++ FlightRouteGuesser.Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightRouteGuesser.Core.Entities
+{
+    public class BaseEntity
+    {
+        protected BaseEntity() { }
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ChangedDate { get; set; }
+        public Guid ExternalId { get; set; }
+    }
+}
